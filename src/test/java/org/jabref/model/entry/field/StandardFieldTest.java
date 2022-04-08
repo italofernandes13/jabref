19b3803e2d82ddd3bd16a17be9b170bbee844416
italofernandes13 <70165759+italofernandes13@users.noreply.github.com>
2022-04-07 22:53:48 -0300

    Add testes para StandardField

--- src/test/java/org/jabref/model/entry/field/StandardFieldTest.java
+++ src/test/java/org/jabref/model/entry/field/StandardFieldTest.java
@@ -2,12 +2,43 @@ package org.jabref.model.entry.field;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class StandardFieldTest {
 
     @Test
     void fieldsConsideredEqualIfSame() {
         assertEquals(StandardField.TITLE, StandardField.TITLE);
+    }
+
+    @Test
+    void fieldVolumeNaoAceitaLetra() {
+        String teste = String.valueOf(StandardField.VOLUME.validaVolume("T"))
+        assertFalse(teste, Boolean.parseBoolean("T"));
+    }
+    @Test
+    void fieldVolumeNaoAceitaSimbolo() {
+        String teste = String.valueOf(StandardField.VOLUME.validaVolume("@"))
+        assertFalse(teste, Boolean.parseBoolean("@"));
+    }
+    @Test
+    void fieldVolumeAceitaNumero() {
+        String teste = String.valueOf(StandardField.VOLUME.validaVolume("8"))
+        assertEquals(teste, "8");
+    }
+    @Test
+    void fieldVolumeNaoAceitaLetraComNumero() {
+        String teste = String.valueOf(StandardField.VOLUME.validaVolume("2a28b"))
+        assertFalse(teste, Boolean.parseBoolean("2a28b"));
+    }
+    @Test
+    void fieldVolumeNaoAceitaSimboloComNumero() {
+        String teste = String.valueOf(StandardField.VOLUME.validaVolume("2@28#"))
+        assertFalse(teste, Boolean.parseBoolean("2@28#"));
+    }
+    @Test
+    void fieldVolumeAceitaVariosNumero() {
+        String teste = String.valueOf(StandardField.VOLUME.validaVolume("84620"))
+        assertEquals(teste, "84620");
     }
 }
